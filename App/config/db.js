de668734d984ff7ac79e5d5192429adb809fe496
mynkosofia <@email.commynkosofi@gmail.com>
2025-06-24 01:34:07 +0300

    fix: update MongoDB connection string

--- App/config/db.js
+++ App/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb+srv://mynkosofi:admin24021998@briolin.7xxgcm5.mongodb.net/',
+    db: 'mongodb+srv://mynkosofi:admin24021998@briolin.7xxgcm5.mongodb.net/briolin?retryWrites=true&w=majority',
     secret: 'some_secret_key'
 }
--- App/index.js
+++ App/index.js
@@ -15,7 +15,7 @@ var session = require('express-session');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //?
 app.set('trust proxy', 1) // trust first proxy
